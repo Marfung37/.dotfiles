50575168fb42213b502e6625aece2d5756799e58
Marvin Fung <marvin.fung37@gmail.com>
2026-06-24 16:58:32 -0500

    Fixes nvim to use dracula and adds python lsp stuff

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,6 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- tab to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
 vim.o.background = "dark"
 
 vim.g.snacks_animate = false
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,28 +1,20 @@
 return {
-  -- 1. Install the Dracula plugin
   {
     "Mofiqul/dracula.nvim",
-    lazy = false, -- Load it immediately on startup
-    priority = 1000, -- High priority to ensure it loads before other plugins
+    lazy = false,
+    priority = 1000,
     config = function()
-      -- Configure Dracula options here BEFORE loading it
       local dracula = require("dracula")
       dracula.setup({
-        -- If you want transparency alongside Dracula, uncomment the line below:
-        -- transparent = true,
-        show_end_of_buffer = true, -- Shows ~ at the end of buffers
+        transparent_bg = true,
+        show_end_of_buffer = true,
       })
     end,
   },
   {
-    "folke/tokyonight.nvim",
-    lazy = true,
+    "LazyVim/LazyVim",
     opts = {
-      transparent = true, -- Enables transparency for the main editor
-      styles = {
-        sidebars = "transparent", -- Makes Neo-tree transparent
-        floats = "transparent", -- Makes popups/documentation windows transparent
-      },
+      colorscheme = "dracula",
     },
   },
 }
